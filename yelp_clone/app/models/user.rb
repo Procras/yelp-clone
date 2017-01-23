a16c9ffefda7127f78de70cd5a913a1e9f3deadf
Giancarlo <gfabrizi@gmail.com>
2017-01-23 16:05:31 +0100

    resolves omniauth-facebook authentication

--- yelp_clone/app/models/user.rb
+++ yelp_clone/app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   has_many :restaurants
   has_many :reviews
   has_many :reviewed_restaurants, through: :reviews, source: :restaurant
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
@@ -16,13 +15,13 @@ class User < ActiveRecord::Base
     end
   end
 
-  def self.new_with_session(params, session)
-    super.tap do |user|
-     if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-       user.email = data["email"] if user.email.blank?
-     end
-    end
-  end
+  # def self.new_with_session(params, session)
+  #   super.tap do |user|
+  #    if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+  #      user.email = data["email"] if user.email.blank?
+  #    end
+  #   end
+  # end
 
   def has_reviewed?(restaurant)
     reviewed_restaurants.include? restaurant
